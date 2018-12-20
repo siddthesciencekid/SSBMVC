825628220ea95de768398946a1b15eaacc8cf877
Siddhartha Gorti <siddthekid2012@gmail.com>
2018-12-19 23:32:32 -0800

    Stock count working

--- main.py
+++ main.py
@@ -29,11 +29,13 @@ def main():
 
     # Datapoints
     num_frames = 0
+    match_has_started = False
     max_go_matches = 0
     time_start = 0
     char1 = ''
     char2 = ''
 
+    print('FRAME\tPLAYER 1 STOCK COUNT\tPLAYER 2 STOCK COUNT')
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -45,67 +47,49 @@ def main():
             # Use feature matching with ORB to get start match time
             good = compute_ORB_feature_match_for_start(frame)
             if len(good) > MIN_MATCH_COUNT and len(good) > max_go_matches:
+                match_has_started = True
                 max_go_matches = len(good)
                 time_start = num_frames / float(fps)
 
                 frame = frame[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+                cv2.imshow('frame', frame)
+                cv2.waitKey(0)
+
 
 
                 # Once the match starts we can use the current frame to determine the characters being played
                 # for filename in os.listdir('data/characters'):
                     # print(filename)
-                character_template = cv2.imread('data/characters/pikachu-default.png', 1)
-                character_template = cv2.cvtColor(character_template, cv2.COLOR_BGR2GRAY)
-                w, h = character_template.shape[::-1]
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-                found = None
-                # loop over the scales of the image
-                for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-                    print(scale)
-                    # resize the image according to the scale, and keep track
-                    # of the ratio of the resizing
-                    resized = imutils.resize(frame, width=int(frame.shape[1] * scale))
-                    r = frame.shape[1] / float(resized.shape[1])
-
-                    # if the resized image is smaller than the template, then break
-                    # from the loop
-                    if resized.shape[0] < h or resized.shape[1] < w:
-                        break
-
-                    # detect edges in the resized, grayscale image and apply template
-                    # matching to find the template in the image
-                    edged = cv2.Canny(resized, 50, 200)
-                    result = cv2.matchTemplate(edged, character_template, cv2.TM_CCOEFF)
-                    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-
-                    # check to see if the iteration should be visualized
-                    # draw a bounding box around the detected region
-                    clone = np.dstack([edged, edged, edged])
-                    cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                                  (maxLoc[0] + w, maxLoc[1] + h), (0, 0, 255), 2)
-                    cv2.imshow("Visualize", clone)
-                    cv2.waitKey(0)
-
-                    # if we have found a new maximum correlation value, then update
-                    # the bookkeeping variable
-                    if found is None or maxVal > found[0]:
-                        found = (maxVal, maxLoc, r)
-
-                # unpack the bookkeeping variable and compute the (x, y) coordinates
-                # of the bounding box based on the resized ratio
-                (_, maxLoc, r) = found
-                (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-                (endX, endY) = (int((maxLoc[0] + w) * r), int((maxLoc[1] + h) * r))
-
-                # draw a bounding box around the detected result and display the image
-                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-                cv2.imshow("Image", frame)
-                cv2.waitKey(0)
+                # character_template = cv2.imread('data/characters/pikachu-capture.png', 1)
+                # character_template = cv2.cvtColor(character_template, cv2.COLOR_BGR2GRAY)
+                # w, h = character_template.shape[::-1]
+                # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                #
+                # res = cv2.matchTemplate(frame, character_template, cv2.TM_CCOEFF_NORMED)
+                #
+                # # Specify a threshold
+                # threshold = 0.7
+                #
+                # # Store the coordinates of matched area in a numpy array
+                # loc = np.where(res >= threshold)
+                #
+                # found = set()
+                # # Draw a rectangle around the matched region.
+                # for pt in zip(*loc[::-1]):
+                #     cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+                #     sensitivity = 43
+                #     found.add((round(pt[0] / sensitivity), round(pt[1] / sensitivity)))
 
 
 
-                    #cv2.imshow('frame', frame)
+                #cv2.imshow('Detected', frame)
+                #cv2.waitKey(0)
+
+            if match_has_started and num_frames % 10 == 0:
+                compute_and_print_num_stocks('data/characters/fox-capture.png', 'data/characters/pikachu-capture.png', frame, num_frames)
+
+
+            cv2.imshow('frame', frame)
             if cv2.waitKey(2) & 0xFF == ord('q'):
                 break
         else:
@@ -131,6 +115,40 @@ def compute_ORB_feature_match_for_start(frame):
         if m.distance < 11:
             good.append([m])
     return good
+
+def compute_and_print_num_stocks(player1_stock_image, player2_stock_image, frame, frame_num):
+    frame = frame[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+
+    character_template1 = cv2.imread(player1_stock_image, 1)
+    character_template1 = cv2.cvtColor(character_template1, cv2.COLOR_BGR2GRAY)
+
+    character_template2 = cv2.imread(player2_stock_image, 1)
+    character_template2 = cv2.cvtColor(character_template2, cv2.COLOR_BGR2GRAY)
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    res1 = cv2.matchTemplate(frame, character_template1, cv2.TM_CCOEFF_NORMED)
+    res2 = cv2.matchTemplate(frame, character_template2, cv2.TM_CCOEFF_NORMED)
+
+    # Specify a threshold
+    threshold = 0.80
+
+    # Store the coordinates of matched area in a numpy array
+    loc1 = np.where(res1 >= threshold)
+    loc2 = np.where(res2 >= threshold)
+
+    player1_found = set()
+    player2_found = set()
+    sensitivity = 43
+
+    for pt in zip(*loc1[::-1]):
+        player1_found.add((round(pt[0] / sensitivity), round(pt[1] / sensitivity)))
+
+    for pt in zip(*loc2[::-1]):
+        player2_found.add((round(pt[0] / sensitivity), round(pt[1] / sensitivity)))
+
+    print(str(frame_num) + '\t' + str(len(player1_found)) + '\t' + str(len(player2_found)))
+
 
 
 if __name__ == "__main__":
